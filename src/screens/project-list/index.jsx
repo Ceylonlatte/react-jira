5f8e1ecfd217e28543bb9554154901e707f1c127
刘鑫沛 <lxp1@meitu.com>
2021-05-23 00:47:37 +0800

    ci:remove husky

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import {SearchPanel} from "./search-panel";
 import {List} from "./list";
 import {useEffect, useState} from "react";
-import {cleanObject} from "../../utils";
+import {cleanObject, useDebounce, useMount} from "../../utils";
 import * as qs from 'qs';
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -14,24 +14,24 @@ export const ProjectListScreen = () => {
         name: '',
         personId: ''
     })
+    const [list, setList] = useState([])
+    const debounceParam = useDebounce(param, 2000)
 
-    const [list, setList] = useState([]
-    )
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
             if (response.ok) {
                 setList(await response.json())
             }
         })
-    }, [param])
+    }, [debounceParam])
 
-    useEffect(() => {
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async response => {
             if (response.ok) {
                 setUsers(await response.json())
             }
         })
-    }, [])
+    })
 
 
     return <div>
