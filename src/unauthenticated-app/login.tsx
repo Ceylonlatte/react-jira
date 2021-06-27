836870c2ade8559627023c30bea453412c4d86c5
刘鑫沛 <lxp1@meitu.com>
2021-06-27 18:02:51 +0800

    :lipstick:  引入antd组件库

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,31 +1,33 @@
 import React, { FormEvent } from "react";
 import { useAuth } from "context/auth-context";
+import { Form, Input, Button } from "antd";
 
 export const LoginScreen = () => {
   const { login, user } = useAuth();
 
   // HTMLFormElement extends Element
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const username = (event.currentTarget.elements[0] as HTMLInputElement)
-      .value;
-    const password = (event.currentTarget.elements[1] as HTMLInputElement)
-      .value;
-    login({ username, password });
+  const handleSubmit = (values: { username: string; password: string }) => {
+    login(values);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onFinish={handleSubmit}>
       {user ? <div>登陆成功,用户名： {user?.name}</div> : null}
-      <div>
-        <label htmlFor="username">用户名</label>
-        <input type="text" id={"username"} />
-      </div>
-      <div>
-        <label htmlFor="password">密码</label>
-        <input type="password" id={"password"} />
-      </div>
-      <button type={"submit"}>登录</button>
-    </form>
+      <Form.Item
+        name={"username"}
+        rules={[{ required: true, message: "请输入用户名" }]}
+      >
+        <Input type="text" id={"username"} />
+      </Form.Item>
+      <Form.Item
+        name={"password"}
+        rules={[{ required: true, message: "请输入密码" }]}
+      >
+        <Input type="password" id={"password"} />
+      </Form.Item>
+      <Button htmlType={"submit"} type={"primary"}>
+        登录
+      </Button>
+    </Form>
   );
 };
